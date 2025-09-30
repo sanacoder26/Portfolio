7ec8dcc0f5175efd4e2f626cbc88e114693ae3c7
sanacoder26 <wcoding819@gmail.com>
2025-09-30 20:09:07 +0500

    commit

---
+++ app.js
@@ -0,0 +1,28 @@
+// Typing effect for hero section
+const roles = ["Web Designer", "Frontend Developer"];
+let roleIndex = 0;
+let charIndex = 0;
+const typingElement = document.getElementById("typing");
+
+function typeEffect() {
+  if (charIndex < roles[roleIndex].length) {
+    typingElement.textContent += roles[roleIndex].charAt(charIndex);
+    charIndex++;
+    setTimeout(typeEffect, 100);
+  } else {
+    setTimeout(eraseEffect, 1500);
+  }
+}
+
+function eraseEffect() {
+  if (charIndex > 0) {
+    typingElement.textContent = roles[roleIndex].substring(0, charIndex - 1);
+    charIndex--;
+    setTimeout(eraseEffect, 50);
+  } else {
+    roleIndex = (roleIndex + 1) % roles.length;
+    setTimeout(typeEffect, 500);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", typeEffect);
